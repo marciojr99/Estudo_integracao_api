fb56952cf491760de66a39e91d58069815db4869
Márcio Araújo <94808661+marciojr99@users.noreply.github.com>
2022-04-19 14:34:46 -0300

    Update cotação.py
    adicionada variavel que obtem a data de obtenção da cotação e mudada mensagem enviada pela api com inclusão do dado de data

--- cotação.py
+++ cotação.py
@@ -1,4 +1,5 @@
 import requests
+
 from twilio.rest import Client
 #gera a requisição para a api gratis em questão( requisição recebe dado em formato json
 req = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
@@ -6,8 +7,14 @@ req = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-
 print(req)
 print(req.json())
 req = req.json()
+
 #dolar recebe o dicionário json recebido atravez da requisição e armazena os parametros "USDBRL" e "bid"
+
+#data recebe o dicionário json recebido atravez da requisição e armazena os parametros "USDBRL" e "create_date"
+
 dolar = req['USDBRL']['bid']
+data=req['USDBRL']['create_date']
+print(dolar, data)
 
 
 
@@ -18,15 +25,15 @@ dolar = req['USDBRL']['bid']
 account_sid = "AC4286d6630ed49281f2903b3094f8e3e5"
 
 #token autenticação twilio
-auth_token = "eff07e1c9b7ae6cf5147609acb48852a"
+auth_token = "909f884424c09b290ca67f36bbf36d85"
 
 client = Client(account_sid, auth_token)
 
-#bloco de codigo usado para enviar o dado obtido na api de requisição
+#bloco de codigo usado para enviar os dados obtido na api de requisição
 message = client.messages.create(
     to="+5511991306070",
     from_="+18304686152",
-    body=f" {dolar} ")
+    body=f"  cotação do dolar na data e hora de  {data} é:  {dolar} ")
 
 print(message.sid)
 
